ce3b0572eb4b91f036cbb94ad60ba24103025027
navadeep1251 <navdp.net@gmail.com>
2020-11-06 10:22:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-rxjs-nav-9s4i64
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rxjs-nav-9s4i64)
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, VERSION } from "@angular/core";
+import { from, of } from "rxjs";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+
+  ngOnInit() {
+    of(1, 2, 3).subscribe(console.log);
+    from([1, 2, 3]).subscribe(console.log);
+  }
+}
